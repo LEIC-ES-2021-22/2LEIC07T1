fdb96d87719f913c58ffa43b3af05f2c3d4d0717
José Luís Cunha Rodrigues <joseluiscunharodrigues@gmail.com>
2022-03-31 23:10:12 +0100

    testing lint

--- uniteca/lib/main.dart
+++ uniteca/lib/main.dart
@@ -92,11 +92,11 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,                       
           children: 
           <Widget>[ 
             Text(
-              'You have pushed the button this many times:',
+                                                                 'You have pushed the button this many times:',
               ),
             Text(
               '$_counter',
