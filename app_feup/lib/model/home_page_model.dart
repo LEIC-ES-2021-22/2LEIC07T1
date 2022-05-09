8c03bc5d56f6afad18c185324e6eb3de8e9ca82d
Francisco Nunes <francisconunes01@gmail.com>
2022-05-10 00:22:53 +0100

    Fixed lint warnings

--- app_feup/lib/model/home_page_model.dart
+++ app_feup/lib/model/home_page_model.dart
@@ -1,1 +1,2 @@
-enum FAVORITE_WIDGET_TYPE { exams, schedule, printBalance, account, busStops, libraryOccupation}
+enum FAVORITE_WIDGET_TYPE
+{exams, schedule, printBalance, account, busStops,libraryOccupation}
