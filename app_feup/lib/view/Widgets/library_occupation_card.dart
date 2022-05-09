597ca57b383ba3933d2f51290227f50c72a2e276
José Luís Cunha Rodrigues <joseluiscunharodrigues@gmail.com>
2022-05-09 16:54:30 +0100

    Added occupation percent indicator

--- app_feup/lib/view/Widgets/library_occupation_card.dart
+++ app_feup/lib/view/Widgets/library_occupation_card.dart
@@ -1,49 +1,62 @@
-
 import 'package:uni/model/app_state.dart';
 import 'package:tuple/tuple.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:uni/view/Widgets/request_dependent_widget_builder.dart';
 import 'generic_card.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
-/// Manages the exam card section inside the personal area.
+/// Manages the library card section inside the personal area.
 class LibraryOccupation extends GenericCard {
   LibraryOccupation({Key key}) : super(key: key);
 
-  LibraryOccupation.fromEditingInformation(Key key, bool editingMode,
-      Function onDelete)
+  LibraryOccupation.fromEditingInformation(
+      Key key, bool editingMode, Function onDelete)
       : super.fromEditingInformation(key, editingMode, onDelete);
 
   @override
   String getTitle() => 'Ocupação da biblioteca';
 
   @override
-  onClick(BuildContext context) =>
-      null;
+  onClick(BuildContext context) => null;
 
   @override
   Widget buildCardContent(BuildContext context) {
-    return StoreConnector<AppState, Tuple2<String, RequestStatus>> (
-        converter: (store) => Tuple2('', // TODO: Issue #390
+    return StoreConnector<AppState, Tuple2<String, RequestStatus>>(
+        converter: (store) => Tuple2(
+            'one',
             RequestStatus.none),
-        builder: (context, canteen) {
+        builder: (context, occupation) {
           return RequestDependentWidgetBuilder(
               context: context,
-              status: canteen.item2,
+              status: occupation.item2,
               contentGenerator: generateOccupation,
-              content: canteen.item1,
-              contentChecker:
-              canteen.item1 != null && canteen.item1.isNotEmpty,
+              content: occupation.item1,
+              contentChecker: 
+                occupation.item1 != null && occupation.item1.isNotEmpty,
               onNullContent: Center(
-                  child: Text('Não existem cantinas para apresentar',
+                  child: Text('Não existem dados para apresentar',
                       style: Theme.of(context).textTheme.headline4,
                       textAlign: TextAlign.center)));
         });
   }
 
   Widget generateOccupation(occupations, context) {
-    return Text("occupation");
+    return CircularPercentIndicator(
+      radius: 60.0,
+      lineWidth: 7.0,
+      percent: 0.41,
+      center: Text(
+        '41.0%',
+        style: Theme.of(context).textTheme.headline2
+      ),
+      footer: Text(
+        '200/488',
+        style: Theme.of(context).textTheme.headline2
+      ),
+      circularStrokeCap: CircularStrokeCap.round,
+      backgroundColor: Theme.of(context).hintColor,
+      progressColor: Theme.of(context).accentColor,
+    );
   }
-
-
 }
