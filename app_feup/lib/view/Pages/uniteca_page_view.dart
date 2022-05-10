951ccbd3b9c694482f228a66e7659c83b7147c5b
Guilherme Almeida <72418555+theguilhermealmeida@users.noreply.github.com>
2022-05-10 16:08:01 +0100

    Resolved repeated code´

--- app_feup/lib/view/Pages/uniteca_page_view.dart
+++ app_feup/lib/view/Pages/uniteca_page_view.dart
@@ -24,104 +24,68 @@ class UnitecaPageViewState extends GeneralPageViewState {
 
           PageTitle(name: 'Floors'),
 
-          getFloors(1, 42.0, 87.2),
-          getFloors(3, 23.0, 36.8),
-          getFloors(5, 70.3, 12.0),
+          getFloors(1, 2, 42.0, 87.2),
+          getFloors(3, 4, 23.0, 36.8),
+          getFloors(5, 6, 70.3, 12.0),
       ]);
   }
 
-  Widget getFloors(int floor, double percentage1, double percentage2) {
+  Widget getFloors(int floor1, int floor2, double percentage1, double percentage2) {
      return Row(
         children: [
-
-          Container (
-
-            child:
-            Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                height: 150.0,
-                width: 150.0,
-                padding: EdgeInsets.all(20.0),
-                decoration: BoxDecoration (
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    color: Theme.of(context).primaryColor,
-                    boxShadow: [
-                        BoxShadow(
-                            color: Color.fromARGB(0x1c, 0, 0, 0),
-                            blurRadius: 7.0,
-                            offset: Offset(0.0, 1.0),
-                        )
-                    ]
-                             ),
-                child:
-                    Column(
-                          children: [
-                              Text('Floor ' + floor.toString()),
-                              Padding(padding: EdgeInsets.all(15)),
-                              Text(percentage1.toString() + '%',
-                                  style:
-                                       TextStyle(
-                                           fontSize: 22,
-                                           fontWeight: FontWeight.bold)),
-                              Padding(padding: EdgeInsets.only(top: 28)),
-                              Container(
-                                        child: LinearPercentIndicator(
-                                              lineHeight: 7.0,
-                                              percent: percentage1 / 100,
-                                              progressColor:
-                                                    Theme.of(context).
-                                                    accentColor,
-                                        )
-                              )
-                          ]
-                    ),
-            ),
-          ),
-
+          getFloorsAux(floor1, percentage1),
           Padding(padding: EdgeInsets.only(left: 12)),
-
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                        height: 150.0,
-                        width: 150.0,
-                        padding: EdgeInsets.all(20.0),
-                        decoration: BoxDecoration (
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                                          color: Theme.of(context).primaryColor,
-                                          boxShadow: [
-                                                BoxShadow(
-                                                color:
-                                                    Color.
-                                                    fromARGB(0x1c, 0, 0, 0),
-                                                blurRadius: 7.0,
-                                                offset: Offset(0.0, 1.0),
-                                                )
-                                          ]
-                        ),
-             child:
-                  Column(
-                      children: [
-                            Text('Floor ' + (floor + 1).toString()),
-                            Padding(padding: EdgeInsets.all(15)),
-                            Text(percentage2.toString() + '%',
-                                style:
-                                    TextStyle(
-                                        fontSize: 22,
-                                        fontWeight: FontWeight.bold)),
-                            Padding(padding: EdgeInsets.only(top: 28)),
-                            Container(
-                                    child: LinearPercentIndicator(
-                                            lineHeight: 7.0,
-                                            percent: percentage2 / 100,
-                                            progressColor:
-                                                    Theme.of(context).
-                                                    accentColor,
-                                    )
-                            )
-                    ]
-                   ),
-          ),
+          getFloorsAux(floor2, percentage2),
         ]
      );
+  }
+
+
+
+  Widget getFloorsAux(int floor, double percentage) {
+    return
+      Container (
+
+        child:
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          height: 150.0,
+          width: 150.0,
+          padding: EdgeInsets.all(20.0),
+          decoration: BoxDecoration (
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              color: Theme.of(context).primaryColor,
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(0x1c, 0, 0, 0),
+                  blurRadius: 7.0,
+                  offset: Offset(0.0, 1.0),
+                )
+              ]
+          ),
+          child:
+          Column(
+              children: [
+                Text('Floor ' + floor.toString()),
+                Padding(padding: EdgeInsets.all(15)),
+                Text(percentage.toString() + '%',
+                    style:
+                    TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold)),
+                Padding(padding: EdgeInsets.only(top: 28)),
+                Container(
+                    child: LinearPercentIndicator(
+                      lineHeight: 7.0,
+                      percent: percentage / 100,
+                      progressColor:
+                      Theme.of(context).
+                      accentColor,
+                    )
+                )
+              ]
+          ),
+        ),
+      );
   }
 }
