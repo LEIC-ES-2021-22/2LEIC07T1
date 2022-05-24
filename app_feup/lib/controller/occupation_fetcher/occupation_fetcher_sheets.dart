f9650eb046296de59fc2093ecfefa1252714479f
José Luís Cunha Rodrigues <joseluiscunharodrigues@gmail.com>
2022-05-24 14:50:56 +0100

    Unit tests for the occupation

--- app_feup/lib/controller/occupation_fetcher/occupation_fetcher_sheets.dart
+++ app_feup/lib/controller/occupation_fetcher/occupation_fetcher_sheets.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:uni/model/app_state.dart';
 import 'package:redux/redux.dart';
 import 'package:uni/model/entities/library.dart';
+import 'package:uni/controller/parsers/parser_occupation.dart';
 import 'package:gsheets/gsheets.dart';
 
 Future<String> getJson() {
@@ -22,18 +23,7 @@ class OccupationFetcherSheets {
 
       final sheet = ss.worksheetByTitle('MANUAL');
 
-      final occupation = LibraryOccupation();
-
-      for (int i = 1; i < 7; i++) {
-          final occupationCell = await sheet.cells.cell(column: 3, row: i + 1);
-          final capacityCell = await sheet.cells.cell(column: 5, row: i + 1);
-
-          occupation.addFloor(FloorOccupation(
-            i, int.parse(occupationCell.value), int.parse(capacityCell.value)));
-      }
-      return occupation;
-    } on FormatException {
-      return LibraryOccupation();
+      return getOccupationFromSheet(sheet);
     } catch (FlutterError) {
       return LibraryOccupation();
     }
