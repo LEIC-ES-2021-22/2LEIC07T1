6db9714e27ea13db320ff9a6163bc2ffc6696624
Francisco Nunes <francisconunes01@gmail.com>
2022-06-07 16:51:55 +0100

    Histogram

--- app_feup/lib/view/Pages/uniteca_page_view.dart
+++ app_feup/lib/view/Pages/uniteca_page_view.dart
@@ -8,6 +8,8 @@ import 'package:uni/view/Widgets/library_occupation_card.dart';
 import 'package:uni/view/Widgets/page_title.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:uni/view/Widgets/request_dependent_widget_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class UnitecaPageView extends StatefulWidget {
   @override
@@ -43,12 +45,38 @@ class UnitecaPageViewState extends SecondaryPageViewState {
         children: <Widget>[
           PageTitle(name: 'Uniteca'),
           LibraryOccupationCard(),
+          PageTitle(name: "Usual Occupation"),
+          histogram(),
           PageTitle(name: 'Pisos'),
           createFloorRow(occupation.getFloor(1), occupation.getFloor(2)),
           createFloorRow(occupation.getFloor(3), occupation.getFloor(4)),
           createFloorRow(occupation.getFloor(5), occupation.getFloor(6))
         ]);
   }
+
+  Widget histogram(){
+    return Container(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: 150.0,
+        width: 12.0,
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            color: Theme.of(context).primaryColor,
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromARGB(0x1c, 0, 0, 0),
+                blurRadius: 7.0,
+                offset: Offset(0.0, 1.0),
+              )
+            ]),
+
+       
+      ),
+    );
+  }
+
 
   Widget createFloorRow(FloorOccupation floor1, FloorOccupation floor2) {
     return Row(
@@ -96,5 +124,33 @@ class UnitecaPageViewState extends SecondaryPageViewState {
           ]),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child:SfCartesianChart(
+          title: ChartTitle(text: 'Flutter Chart'),
+          legend: Legend(isVisible: true),
+          series: getDefaultData(),
+        )
+    );
+  }
+  static List<LineSeries<Tuple2, num>> getDefaultData() {
+    final List<Tuple2> chartData = <Tuple2>[
+
+    ];
+    return <LineSeries<Tuple2, num>>[
+      LineSeries<Tuple2, num>(
+          dataSource: chartData,
+          xValueMapper: (Tuple2 sales, _) => sales.item1,
+          yValueMapper: (Tuple2 sales, _) => sales.item2,
+          markerSettings: MarkerSettings(
+
+              borderWidth: 3,
+              borderColor: Colors.red),
+      )];
+
+
   }
 }
